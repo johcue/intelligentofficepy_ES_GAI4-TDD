66e21dc5554f059e7c3b9237882cfc4771ef67f3
johcue <lianagar914@gmail.com>
2025-12-19 09:40:32 +0000

    US2,1 RED Refactor weekday

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -41,7 +41,7 @@ class TestIntelligentOffice(unittest.TestCase):
 
     @patch.object(SDL_DS3231, "read_datetime")
     def test_blinds_fully_open_weekday(self, mock_datetime: Mock):
-        mock_datetime.return_value = datetime(2025, 12, 19, 7, 10)
+        mock_datetime.return_value = datetime(2025, 12, 19, 8, 10)
         io = IntelligentOffice()
         io.manage_blinds_based_on_time()
         self.assertTrue(io.blinds_open)
